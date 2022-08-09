72eda7383f5553663a8cbf3ac9908145a4f2a163
Rogério <rlrb71@gmail.com>
2022-08-09 12:12:00 -0300

    fixed version github

--- src/routes/api/wms/palavras-chaves/count.js
+++ src/routes/api/wms/palavras-chaves/count.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import {wmsKeywordObj} from "$lib/inde/wms/data/json_keywords";
-=======
-import {wmsKeywordObj} from "$lib/ogc/wms/data/json_keywords";
->>>>>>> 2c3dfee91367745607d20563049749a9ef6cf1e2
 
+import {wmsKeywordObj} from "$lib/inde/wms/data/json_keywords";
 export async function get({ request }) {
     return {
          status: 200,
--- src/routes/api/wms/palavras-chaves/index.js
+++ src/routes/api/wms/palavras-chaves/index.js
@@ -1,10 +1,9 @@
 import { fetchAsWMSCapabilitiesObject } from "$lib/ogc/wms/WMSCapabilitiesObject";
 import { catalogos_servicos } from "$lib/inde/CatalogoINDE";
-<<<<<<< HEAD
+
 import {wmsKeywordObj} from "$lib/inde/wms/data/json_keywords";
-=======
-import {wmsKeywordObj} from "$lib/ogc/wms/data/json_keywords";
->>>>>>> 2c3dfee91367745607d20563049749a9ef6cf1e2
+
+
 export async function get({ request }) {
      return {
           status: 200,
